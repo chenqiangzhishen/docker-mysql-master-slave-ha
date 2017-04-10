e53f9e49e4365e15e86029deac59ae287b43a564
chenqiang41 <chenqiang41@wanda.cn>
2017-04-10 19:58:00 +0800

    fix to make keepalived lifecycle synced with monitor service
    Signed-off-by: chenqiang41 <chenqiang41@wanda.cn>

--- check-mysql-status.sh
+++ check-mysql-status.sh
@@ -7,13 +7,19 @@
 
 # check the mysql service for three times,
 # if mysql service is really dead, should kill the keepalived.
-for check_times in `seq 1 3`; do
-    sudo netstat -anp | grep 13388 | grep LISTEN > /dev/null 2>&1
-    if [[ $? -eq 1 ]]; then
-        sleep 1
-        if [ $check_times -eq 3 ]; then
-            sudo pkill keepalived
-        fi
-    fi
-done
+
+# the check script will check the following cmd return code,
+# so if true, the keepalived in the node will take effect, or will be demoted.
+# and VIP will shift to slave node.
+sudo netstat -anp | grep 13388 | grep LISTEN > /dev/null 2>&1
+
+# for check_times in `seq 1 3`; do
+#     sudo netstat -anp | grep 13388 | grep LISTEN > /dev/null 2>&1
+#     if [[ $? -eq 1 ]]; then
+#         sleep 1
+#         if [ $check_times -eq 3 ]; then
+#             sudo pkill keepalived
+#         fi
+#     fi
+# done
 
